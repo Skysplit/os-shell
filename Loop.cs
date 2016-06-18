033faba28abad49b90f8247c3a1c2c448e94eb70
Tomek Romik <manetamajster@gmail.com>
2016-06-18 21:46:51 +0200

    Fix redirecting output. Add some more comments to code

--- Loop.cs
+++ Loop.cs
@@ -78,8 +78,19 @@ namespace ShellApplication
                 return 0;
             }
 
+            // First argument is command name
             string CmdName = Arguments[0];
+
+            // Remove command name from arguments array
             Arguments.RemoveAt(0);
+
+            // Flag for executing process in background
+            bool ExecuteInBackground = false;
+
+            // Set default stdin, stdout  and stderr
+            TextWriter stdout = Console.Out;
+            TextReader stdin = Console.In;
+            TextWriter stderr = Console.Error;
 
             // Check if last argument is an ampersand (&)
             if (Arguments.Count > 0 && Arguments[Arguments.Count - 1] == "&")
@@ -87,34 +98,49 @@ namespace ShellApplication
                 // Do not treat ampersand as argument
                 Arguments.RemoveAt(Arguments.Count - 1);
 
-                // Execute process in background using null stdin, stdout and stderr
-                this.ExecuteBackground(CmdName, TextWriter.Null, TextReader.Null, TextWriter.Null, Arguments.ToArray());
-                return 0;
+                // Set process to be executed in background
+                ExecuteInBackground = true;
+
+                // Set default background process stdin and stderr
+                stdout = TextWriter.Null;
+                stdin = TextReader.Null;
+                stderr = TextWriter.Null;
             }
 
-            // Setup default stdin, stdout and stderr
-            TextWriter stdout = Console.Out;
-            TextReader stdin = Console.In;
-            TextWriter stderr = Console.Error;
-
+            // Check for input/output redirection characters
             foreach (string streamChar in new string[] { "<", ">" })
             {
+                // Check if there are at least two arguments
                 if (Arguments.Count >= 2) {
                     int index = Arguments.LastIndexOf(streamChar);
 
+                    // Check if arrow character is one index before the last one
+                    // Continue loop if it is not
                     if (index == -1 || index == Arguments.Count - 1)
                     {
                         continue;
                     }
 
+                    // Last argument is file path
                     string filePath = Arguments[index + 1];
 
+                    // Check if file exits
                     if (!File.Exists(filePath))
                     {
-                        Console.WriteLine(string.Format("File {0} not exists", filePath));
-                        return 1;
+                        // If file does not exists and we are setting it as stdout, create new file
+                        // Otherwise return an error
+                        if (streamChar == ">")
+                        {
+                            File.Create(filePath).Close();
+                        }
+                        else
+                        {
+                            Console.Error.WriteLine(string.Format("File {0} not exists", filePath));
+                            return 1;
+                        }
                     }
 
+                    // Finally, set proper streams for stdin/stdout
                     if (streamChar == "<")
                     {
                         stdin = new StreamReader(filePath);
@@ -123,6 +149,10 @@ namespace ShellApplication
                     {
                         stdout = new StreamWriter(filePath);
                     }
+
+                    // Remove file and arrow character from arguments array
+                    Arguments.RemoveAt(index + 1);
+                    Arguments.RemoveAt(index);
                 }
                 else
                 {
@@ -132,11 +162,25 @@ namespace ShellApplication
 
             try
             {
+                // Check if process is set to be executed in background
+                if (ExecuteInBackground)
+                {
+                    this.ExecuteBackground(CmdName, stdout, stdin, stderr, Arguments.ToArray());
+                    return 0;
+                }
+
+                // Get exit code
                 return this.ExecuteForeground(CmdName, stdout, stdin, stderr, Arguments.ToArray());
             }
             catch (Exception e)
             {
-                Console.WriteLine(e.Message);
+                // Write error message to stderr
+                stderr.WriteLine(e.Message);
+
+                // Close streams
+                this.CloseStreams(stdout, stdin, stderr);
+
+                // Return error code
                 return 1;
             }
         }
@@ -155,14 +199,18 @@ namespace ShellApplication
 
         private int ExecuteForeground(string cmd, TextWriter stdout, TextReader stdin, TextWriter stderr, string[] args)
         {
+            int ExitCode;
+
             // Check if command is on list of built-in commands
             if (this.BuiltinCommands.List.ContainsKey(cmd))
             {
                 // Execute built-in command
-                return this.BuiltinCommands.List[cmd].Execute(this, stdout, stdin, stderr, args);
+                ExitCode = this.BuiltinCommands.List[cmd].Execute(this, stdout, stdin, stderr, args);
             }
             else
             {
+                bool isConsoleInput = stdin.Equals(Console.In);
+
                 // Create new external process
                 Process ExternalProcess = new Process();
 
@@ -176,25 +224,52 @@ namespace ShellApplication
                 ExternalProcess.StartInfo.EnvironmentVariables["parent"] = Environment.GetEnvironmentVariable("shell");
                 ExternalProcess.StartInfo.UseShellExecute = false;
 
+                // Always redirect standard output and standard error
+                ExternalProcess.StartInfo.RedirectStandardOutput = true;
+                ExternalProcess.StartInfo.RedirectStandardError = true;
+
+                // Check if stdin is set as console input
+                if (!isConsoleInput)
+                {
+                    ExternalProcess.StartInfo.RedirectStandardInput = true;
+                }
+
                 // Start process
                 ExternalProcess.Start();
 
                 string line;
 
-                // Write to process stdin from defined stdin
-                while ((line = stdin.ReadLine()) != null) {
+                // Write to process stdin from defined stdin if it is not console stdin
+                while (!isConsoleInput && (line = stdin.ReadLine()) != null) {
                     ExternalProcess.StandardInput.WriteLine(line);
                 }
 
                 // Write to defined stdout from process stdout
                 stdout.Write(ExternalProcess.StandardOutput.ReadToEnd());
 
+                // Write to defined stderr from process stderr
+                stderr.Write(ExternalProcess.StandardError.ReadToEnd());
+
                 // Wait for process to exit
                 ExternalProcess.WaitForExit();
 
                 // Return process' exit code
-                return ExternalProcess.ExitCode;
+                ExitCode = ExternalProcess.ExitCode;
             }
+
+            // Close opened streams
+            this.CloseStreams(stdout, stdin, stderr);
+
+            // Return exit code
+            return ExitCode;
+        }
+
+        // Close given stdout, stdin and stder streams
+        private void CloseStreams(TextWriter stdout, TextReader stdin, TextWriter stderr)
+        {
+            stdin.Close();
+            stdout.Close();
+            stderr.Close();
         }
     }
 }
